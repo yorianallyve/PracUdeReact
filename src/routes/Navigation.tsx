8c1d3d7e62b20de511663cb68e484b98ee877f50
AzureAD\YoriaNallyveRiosAcev <yoria@evolutionxp.com>
2022-05-10 10:54:34 -0500

    .

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Navigate } from "react-router-dom";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 
 import logo from '../logo.svg';
 
@@ -12,13 +12,13 @@ export const Navigation = () => {
 
           <ul>
             <li>
-              <NavLink to="home">Home</NavLink>
+              <NavLink to="home" className={({isActive})=>isActive ? 'nav-active':''}>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink to="/about"className={({isActive})=>isActive ? 'nav-active':''}>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users">Users</NavLink>
+              <NavLink to="/users"className={({isActive})=>isActive ? 'nav-active':''}>Users</NavLink>
             </li>
           </ul>
         </nav>
@@ -28,7 +28,7 @@ export const Navigation = () => {
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
           <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/" element={< Navigate to="home" replace />} />
+          <Route path="/*" element={< Navigate to="/home" replace />} />
         </Routes>
 
 
