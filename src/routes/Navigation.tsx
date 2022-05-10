4c0c11d7b56a4b1310cc6edb3be7247821aba5da
AzureAD\YoriaNallyveRiosAcev <yoria@evolutionxp.com>
2022-05-10 09:56:29 -0500

    .

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+
+    </BrowserRouter>
+
+  )
+}
+
+export default Navigation
