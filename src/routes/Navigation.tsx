5caa1c21b3f2102037de6f1572c416fe219f8aa9
AzureAD\YoriaNallyveRiosAcev <yoria@evolutionxp.com>
2022-05-10 10:48:02 -0500

    .

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 
 import logo from '../logo.svg';
@@ -12,7 +12,7 @@ export const Navigation = () => {
 
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="home">Home</NavLink>
             </li>
             <li>
               <NavLink to="/about">About</NavLink>
@@ -22,6 +22,29 @@ export const Navigation = () => {
             </li>
           </ul>
         </nav>
+
+
+        <Routes>
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={< Navigate to="home" replace />} />
+        </Routes>
+
+
+
+
+
+        {/* <Route path="/about">
+          <h1>About</h1>
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route> */}
+
 
       </div>
 
